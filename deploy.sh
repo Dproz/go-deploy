173ea65a12ce3cbeb19619f8c2d561ad402ed80d
prabhakar349 <prabhakar.digumarthi@gmail.com>
2018-06-03 22:20:31 -0400

    Add files via upload

---
+++ deploy.sh
@@ -0,0 +1,54 @@
+#!/bin/sh
+#########################################################
+#
+#  Script to pull the latest code and deploy 
+#
+#
+#
+#
+#########################################################
+
+work_dir=/home/ec2-user
+log_file=/home/ec2-user/log.txt
+
+
+repo=${1}
+repo_url=${2}
+commit=${3}
+
+echo "Working on ${repo} and commit ${commit}" >> ${log_file}
+
+# Delete existing code to clone it fresh
+rm -rf ${work_dir}/${1}
+
+
+# move to the working directory
+cd ${work_dir}
+
+# Clone the repository
+
+git clone ${repo_url}
+
+# change to the repo directory
+cd ${work_dir}/${repo}
+
+git fetch --all
+
+
+git checkout ${commit}
+
+
+# Perform the maven build for the project
+mvn clean install
+
+# Create the docker container
+mvn dockerfile:push
+
+# Now run the app
+docker-compose -f ./src/main/docker/app.yml up -d 
+
+
+
+
+
+
